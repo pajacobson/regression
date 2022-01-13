d35fb0016baa7f5b1abadab4115ec7b8f06e4de4
Matthew Rudd <mbrudd@sewanee.edu>
2022-01-12 21:35:32 -0600

    updates

--- R_scripts/heights.R
+++ R_scripts/heights.R
@@ -2,37 +2,52 @@ library(tidyverse)
 # library(gghighlight)
 
 # read the heights data :
-( heights <- read_csv("../datasets/heights.csv", col_types=cols()) )
-ggplot( gather(heights), aes(value)) + 
-  geom_histogram(binwidth=1) + 
-  facet_wrap(~key) + 
+(heights <- read_csv("../datasets/heights.csv", col_types = cols()))
+ggplot(gather(heights), aes(value)) +
+  geom_histogram(binwidth = 1) +
+  facet_wrap( ~ key) +
   xlab("Heights")
 #ggsave("../images/height_histograms.png",device="png")
 
 mean(heights$Father)
 sd(heights$Father)
 
-mean(heights$Son)
+# this is the null model! -----
+mean(heights$Son) 
 sd(heights$Son)
 
-heights <- heights %>% mutate(Group = round(Father))
-( son.means <- heights %>% 
-    group_by(Group) %>% 
-    summarize(Average = mean(Son)) )
+ggplot(heights, aes(Father, Son)) + geom_point()
+
+heights <- heights %>%
+  mutate(Group = round(Father))
+
+# these provide _point estimates_ for predicted mean responses -----
+# and margins of error via SD
+(son.means <- heights %>%
+    group_by(Group) %>%
+    summarize(Average = mean(Son),
+              SD = sd(Son)))
 # summarize(N = n(), Average = mean(Son), SD = sd(Son)))
 
-(heights.plot <- ggplot(heights, aes(Father,Son)) + geom_point())
+(heights.plot <- ggplot(heights, aes(Father, Son)) +
+    geom_point())
 #ggsave("../images/heights.png",device="png")
 
-ggplot(heights %>% filter(Group!=64), aes(Father,Son, alpha=0.75)) + 
-  geom_point() + 
-  geom_point(data=heights %>% filter(Group==64), aes(Father,Son, alpha=1.0) ) + 
+ggplot() +
+  geom_point(data = heights %>% filter(Group != 64),
+             aes(Father, Son),
+             alpha = 0.2) +
+  geom_point(data = heights %>% filter(Group == 64), aes(Father, Son)) +
   theme(legend.position = "none")
+
 # ggsave("../images/fathers64son.png",device="png")
 
-ggplot(heights %>% filter(Group!=70), aes(Father,Son, alpha=.75)) + 
-  geom_point() + 
-  geom_point(data=heights %>% filter(Group==70), aes(Father,Son, alpha=1.0) ) + 
+ggplot() +
+  geom_point(data = heights %>% filter(Group != 70),
+             aes(Father, Son),
+             alpha =
+               .2) +
+  geom_point(data = heights %>% filter(Group == 70), aes(Father, Son)) +
   theme(legend.position = "none")
 # ggsave("../images/fathers70son.png",device="png")
 
@@ -46,7 +61,7 @@ heights.plot +
 #ggsave("../images/avgsons.png",device="png")
 
 heights.plot + 
-  geom_smooth( method="lm", level=0 ) + 
+  geom_smooth( method="lm", level=0, lwd=1 ) + 
   geom_point(data = son.means, aes(Group, Average), color="red", size=3) 
 #ggsave("../images/avgsons_with_line.png",device="png")
 
@@ -56,4 +71,3 @@ heights.plot +
 
 ( heights.fit <- lm( Son ~ Father, data = heights ))
 summary( heights.fit )
-
